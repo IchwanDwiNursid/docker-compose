a98b1f6b9fc0a87345d8f7b4597f5c8fc92d1f29
Thikpad t480 <idwinursid@gmail.com>
2023-11-15 13:43:17 +0700

    docker compose

---
+++ script.sh
@@ -0,0 +1,24 @@
+docker compose create
+
+docker compose start
+
+docker compose ps 
+
+docker compose ps -a
+
+docker compose stop
+
+docker compose down
+
+docker compose ls
+
+#BIND MOUNTS
+docker compose create 
+
+docker compose start
+
+docker compose stop
+
+#VOLUME 
+docker compose create
+
